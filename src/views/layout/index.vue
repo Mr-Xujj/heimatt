d188a49351b8ce331042c09ca085a4a92d2b847b
Mr-Xujj <XUJJ13071637518@outlook.com>
2020-03-24 21:49:31 +0800

    搜索页面静态布局

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -4,7 +4,7 @@
       <!-- 底部导航栏 -->
       <van-tabbar>
   <van-tabbar-item icon="wap-home-o" to="/home">首页</van-tabbar-item>
-  <van-tabbar-item icon="chat-o" to="/search">搜索</van-tabbar-item>
+  <van-tabbar-item icon="search" to="/search">搜索</van-tabbar-item>
   <van-tabbar-item icon="video-o">视频</van-tabbar-item>
   <van-tabbar-item icon="manager-o" to="/me">我的</van-tabbar-item>
 </van-tabbar>
--- src/views/searchResult/searchResult.vue
+++ src/views/searchResult/searchResult.vue
@@ -6,14 +6,61 @@
         <div class="mylogin">搜索结果</div>
       </template>
     </van-nav-bar>
+    <!-- 新闻区 -->
+    <!-- 下拉刷新 -->
+    <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+  <!-- 上拉加载 -->
+  <van-list v-model="loading" :finished="finished" finished-text="没有更多了"  @load="onLoad">
+  <van-cell v-for="item in 3" :key="item" :title="item" >
+    <template slot="title">
+<h4>标题</h4>
+<van-grid :border="false" :column-num="3">
+  <van-grid-item v-for="(imgitem, imgindex) in 3 " :key="imgindex" >
+    <van-image src="https://img.yzcdn.cn/vant/apple-1.jpg" />
+  </van-grid-item>
+</van-grid>
+<div class="searchBox">
+  <span>作者</span>
+  <span>品论</span>
+  <span>时间</span>
+</div>
+<van-grid :column-num="3">
+  <van-grid-item icon="comment-o" text="品论" />
+  <van-grid-item icon="like-o" text="点赞" />
+  <van-grid-item icon="share" text="分享" />
+</van-grid>
+    </template>
+  </van-cell>
+</van-list>
+</van-pull-refresh>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      // 下拉刷新状态
+      isLoading: false,
+      // 加载状态
+      loading: false,
+      // 完毕状态
+      finished: false,
+      // 加载数据源
+      list: []
+    }
+  },
   methods: {
     back () {
       this.$router.back()
+    },
+    // 下拉刷新
+    onRefresh () {
+
+    },
+    // 加载
+    onLoad () {
+
     }
   }
 }
@@ -21,6 +68,7 @@ export default {
 
 <style lang="less">
  .searchResult{
+   margin-top: 46px;
      .login-nav-bar {
   background-color: #3e9df8;
   .mylogin {
@@ -30,6 +78,20 @@ export default {
 .van-nav-bar .van-icon {
     color: #fff;
 }
+.searchBox span{
+  margin-right: 15px;
+}
+ .van-grid-item__content{
+   flex-direction: row;
+   align-items: center;
+   .van-grid-item__icon {
+     font-size: 18px;
+     line-height: 24px;
+     span{
+       margin-top: 0;
+     }
+   }
+ }
  }
 
 </style>
